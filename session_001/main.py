09149eef559ed1d8f04cffff23968671d190c542
Danial Manavi <dmanavi@live.com>
2023-03-12 09:35:55 -0700

    feat: Session 1 & 2.

---
+++ session_001/main.py
@@ -0,0 +1,22 @@
+
+"""
+This file contains
+multiple lines
+of comment
+describing how
+this file may work
+"""
+
+# print("Danial")
+print(10)
+# print(5.8)
+
+print([1, 2, 3])
+print(type(1))
+print(type("Danial"))
+
+# print("123" + 321) -> error
+print(int("123") + 321)
+
+print(len("Danial"))
+print(len([1, 2, 3]))
